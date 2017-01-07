58c051c29f895087e6233adccccf56604d0fe2dd
Rob Currie <rcurrie@ucsc.edu>
2017-01-06 21:38:44 -0800

    Count of files with link to download receipt and detail, still need to stop events from propogating

--- spinnaker/static/index.jsx
+++ spinnaker/static/index.jsx
@@ -19,12 +19,14 @@ class SubmissionsList extends React.Component {
 	handleSubmissionClick(submission) {
     submission == this.state.expanded ? 
       this.setState({expanded: null}) : this.setState({expanded: submission});
-    // If there is a receipt download it for the user
-    // if (submission.receipt) {
-    //   let receipt = "data:text/tab-separated-values," 
-    //     + encodeURIComponent(submission.receipt);
-    //   window.open(receipt);
-    // }
+  }
+
+  handleDownloadReceipt(submission) {
+    if (submission.receipt) {
+      let receipt = "data:text/tab-separated-values," 
+        + encodeURIComponent(submission.receipt);
+      window.open(receipt);
+    }
   }
 
   render() {
@@ -35,16 +37,22 @@ class SubmissionsList extends React.Component {
 						<cgl-data-table-cell>Created</cgl-data-table-cell>
 						<cgl-data-table-cell>ID</cgl-data-table-cell>
 						<cgl-data-table-cell>Status</cgl-data-table-cell>
+						<cgl-data-table-cell>Files</cgl-data-table-cell>
 					</cgl-data-table-row>
 					{this.state.submissions.map(submission => [
 					<cgl-data-table-row onClick={this.handleSubmissionClick.bind(this, submission)}>
 						<cgl-data-table-cell>{submission.created}</cgl-data-table-cell>
 						<cgl-data-table-cell>{submission.id}</cgl-data-table-cell>
 						<cgl-data-table-cell>{submission.status}</cgl-data-table-cell>
+            <cgl-data-table-cell>
+              <a href="#" onClick={this.handleDownloadReceipt.bind(this, submission)}>
+                {submission.receipt && submission.receipt.split('\t').length}
+              </a>
+            </cgl-data-table-cell>
 					</cgl-data-table-row>,
           <div>
-            {this.state.expanded == submission ?
-              <cgl-data-table-row>
+            {submission.receipt && this.state.expanded == submission ?
+              <cgl-data-table-row onClick={this.handleDownloadReceipt.bind(this, submission)}>
                 <cgl-data-table-cell>
                   <div>{submission.validation_message}</div>
                 </cgl-data-table-cell>
